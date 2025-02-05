9fba4dbb5cc2b891daa2eb2872b4a4c93bdb7178
daudtsaqiif <daudtsaqiif49@gmail.com>
2025-02-05 11:39:02 +0700

    1

--- lib/ui/auth/sign_up_page.dart
+++ lib/ui/auth/sign_up_page.dart
@@ -8,8 +8,243 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  bool isObscureText = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            Text(
+              'Get Started Now 👉🏻',
+              style: welcomeTextStyle,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Text(
+                'Create an account or Sign In to explore about our app',
+                textAlign: TextAlign.center,
+                style: subTextStyle,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                    color: greeyColor,
+                    borderRadius: BorderRadius.circular(100)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: mainColor,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text('Sign Up', style: titleTextStyle),
+                      ),
+                    ),
+                    const SizedBox(width: 30),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/sign-in');
+                      },
+                      child: Text('Sign in', style: thinTextStyle),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 35),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child:
+                  Text('Email', style: defaultTextStyle.copyWith(fontSize: 16)),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'example@gmail.com',
+                  hintStyle: TextStyle(color: greeyColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Text('Password',
+                  style: defaultTextStyle.copyWith(fontSize: 16)),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: '********',
+                  hintStyle: TextStyle(color: greeyColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscureText = !isObscureText;
+                      });
+                    },
+                    icon: isObscureText
+                        ? Icon(
+                            Icons.visibility_off_outlined,
+                          )
+                        : Icon(
+                            Icons.visibility,
+                          ),
+                  ),
+                ),
+                obscureText: isObscureText ? true : false,
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: GestureDetector(
+                onTap: () {
+                  print('Forget Password');
+                },
+                child: Text(
+                  'Forget Password?',
+                  textAlign: TextAlign.end,
+                  style: subTextStyle,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text('Sign In', style: titleTextStyle)),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(width: 20),
+                const Expanded(
+                  child: Divider(
+                    thickness: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    'Or Sign In With',
+                    style: subTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const Expanded(
+                  child: Divider(
+                    thickness: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: greeyColor,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 33,
+                          height: 33,
+                          margin: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            image: DecorationImage(
+                              image: AssetImage('assets/icons/logo_google.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Text(
+                            'Google',
+                            style: defaultTextStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: greeyColor,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 28,
+                          height: 28,
+                          margin: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            image: DecorationImage(
+                              image:
+                                  AssetImage('assets/icons/facebook_logo.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 11.0),
+                          child: Text('Facebook', style: defaultTextStyle),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
